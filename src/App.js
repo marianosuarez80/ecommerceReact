53c257fb077f4f7a439d1991d7bbc173b853cc2e
marianosuarez80 <ms@MacBook-Pro-de-Mariano.local>
2022-09-09 23:01:00 -0300

    login logout

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
 import React from "react";
 import { Header } from "./componentes/Header";
+import { LoginButton } from "./Login";
+import { LogoutButton } from "./Logout";
+import { Perfil } from "./Perfil";
+import { useAuth0 } from "@auth0/auth0-react";
 import { ProductosLista } from "./componentes/Productos/index";
 import 'boxicons'
-import { BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { } from "./componentes/Paginas";
-import {Paginas} from "./componentes/Paginas";
-import {DataProvider} from "./context/Dataprovider";
-import {Carrito} from "./componentes/Carrito";
+import { Paginas } from "./componentes/Paginas";
+import { DataProvider } from "./context/Dataprovider";
+import { Carrito } from "./componentes/Carrito";
 import Footer from "./componentes/Footer/Footer";
 
 function App() {
+  const { isAuthenticated } = useAuth0();
+
   return (
     <DataProvider>
-    <div className="App">
-      <BrowserRouter>
-     
+      <div className="App">
+        <BrowserRouter>
+        {isAuthenticated ? (
+        <>
+          <Perfil />
+          <LogoutButton />
+          </>
+          ) : (
+          <LoginButton />
+          )}
         <Header />
-        <Carrito/>
-        <Paginas/>
-        <Footer/>
-      </BrowserRouter>
+          <Carrito />
+          <Paginas />
+          <Footer />
+        </BrowserRouter>
 
-    </div>
+      </div>
     </DataProvider>
   );
 }
