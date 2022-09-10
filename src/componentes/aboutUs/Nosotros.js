eb07bbc59c63cef8066bddbe0a2a9b233dcc12e4
danieltapy89 <danieltapy@gmail.com>
2022-09-10 00:58:49 -0300

    detalles de footer agregado

--- src/componentes/aboutUs/Nosotros.js
+++ src/componentes/aboutUs/Nosotros.js
@@ -1,48 +1,60 @@
 import React from 'react';
 import pili from "../Footer/pili.jpg";
 import nike from "../Footer/Nike.png";
+import daniel from "../Footer/daniel.jpeg";
+import mariano from "../Footer/mariano.jpeg"
+
 
 const Nosotros = () => {
   return (
     <>
-    <div style={{display: 'grid', gridTemplateColumns:'repeat(auto-fit, minmax(200px,1fr))', gap: '1rem'}}>
+    <div id='hover' style={{display: 'grid', gridTemplateColumns:'repeat(auto-fit, minmax(200px,1fr))', gap: '1rem'}}>
         <div style={{width: '200px', borderRadius: '10px', textAlign:'center',marginBottom:'50px', marginTop:'150px', backgroundColor:'#08a05c', color:'white'}}>
-            <h2>Carlos</h2>
-            <img src={nike}></img>
+            <h2>Carlos Martinez</h2>
+            <img className="abaut" src={nike}></img>
             <h6>Lic. en Sistemas</h6>
-            <p><a style={{color: 'yellow'}} href='https://www.linkedin.com/in/eduardo-encinas?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BVdVeTcawS0igyGvrImMh%2BA%3D%3D' target="_blank">Linkedin</a></p>
-            <p><a style={{color: 'yellow'}} href='https://github.com/pili74' target="_blank">GitHub</a></p>
+            <p><a style={{color: 'yellow'}} href='https://www.linkedin.com/login/es' target="_blank"><i className="bi bi-linkedin" id='lin'></i></a></p>
+            <p><a style={{color: 'yellow'}} href='https://github.com/pili74' target="_blank"><i className="bi bi-github" id='git'></i></a></p>
           
         </div>
+
+
         <div style={{width: '200px', borderRadius: '10px',textAlign:'center',marginBottom:'50px', marginTop:'150px', backgroundColor:'#08a05c', color:'white'}}>
-            <h2>Daniel</h2>
-            <img src={nike}></img>
-            <h6>Lic. en Sistemas</h6>
-            <p><a style={{color: 'yellow'}} href='https://www.linkedin.com/in/eduardo-encinas?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BVdVeTcawS0igyGvrImMh%2BA%3D%3D'target="_blank">Linkedin</a></p>
-            <p><a style={{color: 'yellow'}} href='https://github.com/pili74' target="_blank">GitHub</a></p>
+            <h2>Daniel Romero</h2>
+            <img className="abaut" src={daniel}></img>
+            <h6>Dev. Full Stack</h6>
+            <p><a style={{color: 'yellow'}} href='https://www.linkedin.com/in/daniek-romero-95440b195/'target="_blank"><i className="bi bi-linkedin" id='lin'></i></a></p>
+            <p><a style={{color: 'yellow'}} href='https://github.com/danieltapy89' target="_blank"><i className="bi bi-github" id='git'></i></a></p>
            
         </div>
+
+
         <div style={{width: '200px', borderRadius: '10px', textAlign:'center',marginBottom:'50px', marginTop:'150px', backgroundColor:'#08a05c', color:'white'}}>
             <h2>Eduardo Encinas</h2>
             <img className="abaut" src={pili}></img>
             <h6>Lic. en Sistemas</h6>
-            <p><a style={{color: 'yellow'}} href='https://www.linkedin.com/in/eduardo-encinas?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BVdVeTcawS0igyGvrImMh%2BA%3D%3D' target="_blank">Linkedin</a></p>
-            <p><a style={{color: 'yellow'}} href='https://github.com/pili74' target="_blank">GitHub</a></p>
+            <p><a style={{color: 'yellow'}} href='https://www.linkedin.com/in/eduardo-encinas' target="_blank"><i className="bi bi-linkedin" id='lin'></i></a></p>
+            <p><a style={{color: 'yellow'}} href='https://github.com/pili74' target="_blank"><i className="bi bi-github" id='git'></i></a></p>
+
         </div>
+
+
         <div style={{width: '200px', borderRadius: '10px', textAlign:'center',marginBottom:'50px', marginTop:'150px', backgroundColor:'#08a05c', color:'white'}}>
-            <h2>Gaston</h2>
-            <img src={nike}></img>
+            <h2>Gaston Talavera</h2>
+            <img className="abaut" src={nike}></img>
             <h6>Lic. en Sistemas</h6>
-            <p><a style={{color: 'yellow'}} href='https://www.linkedin.com/in/eduardo-encinas?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BVdVeTcawS0igyGvrImMh%2BA%3D%3D'target="_blank">Linkedin</a></p>
-            <p><a style={{color: 'yellow'}} href='https://github.com/pili74'target="_blank">GitHub</a></p>
+            <p><a style={{color: 'yellow'}} href='https://www.linkedin.com/login/es'target="_blank"><i className="bi bi-linkedin" id='lin'></i></a></p>
+            <p><a style={{color: 'yellow'}} href='https://github.com/pili74'target="_blank"><i className="bi bi-github" id='git'></i></a></p>
            
         </div>
+
+
         <div style={{width: '200px', borderRadius: '10px', textAlign:'center',marginBottom:'50px', marginTop:'150px', backgroundColor:'#08a05c', color:'white'}}>
-            <h2>Mariano</h2>
-            <img src={nike}></img>
+            <h2>Mariano Suarez</h2>
+            <img className="abaut" src={mariano}></img>
             <h6>Lic. en Sistemas</h6>
-            <p><a style={{color: 'yellow'}} href='https://www.linkedin.com/in/eduardo-encinas?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BVdVeTcawS0igyGvrImMh%2BA%3D%3D'target="_blank">Linkedin</a></p>
-            <p><a style={{color: 'yellow'}} href='https://github.com/pili74'target="_blank">GitHub</a></p>
+            <p><a style={{color: 'yellow'}} href='http://www.linkedin.com/in/carlos-mariano-a-suarez-928875119'target="_blank"><i className="bi bi-linkedin" id='lin'></i></a></p>
+            <p><a style={{color: 'yellow'}} href='https://github.com/marianosuarez80'target="_blank"><i className="bi bi-github" id='git'></i></a></p>
            
         </div>
     </div>
